77d2f0cc38cce396441b7bdf4b0de2e496fefdc6
SleepToWin <bradford.wight@gmail.com>
2017-10-26 15:15:14 -0400

    Add files via upload

---
+++ CSVReaderWriter.cs
@@ -0,0 +1,96 @@
+﻿/* Based on https://www.codeproject.com/Articles/415732/Reading-and-Writing-CSV-Files-in-Csharp
+ * Modified by Brad Wight
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSV_ReadWrite
+{
+	// Class to hold one csv row
+	public class CsvRow : List<string>
+	{
+		public string RowText { get; set; }
+	}
+
+	// Class to read from a csv file
+	public class CsvReader : StreamReader
+	{
+		public CsvReader(string file) : base(file)
+		{
+		}
+		public CsvReader(Stream stream) : base(stream)
+		{
+		}
+
+		// Read a row from the file
+		public bool ReadRow(CsvRow row)
+		{
+			row.RowText = ReadLine ();
+
+			if (String.IsNullOrEmpty (row.RowText))
+				return false;
+
+			int rows = 0;
+			int position = 0;
+
+			while (position < row.RowText.Length)
+			{
+				string value;
+
+				// Parse a word
+				int start = position;
+				while (position < row.RowText.Length && row.RowText [position] != ',')
+					position++;
+				value = row.RowText.Substring (start, position - start);
+
+				// Add a field to the list
+				if (rows < row.Count)
+					row [rows] = value;
+				else
+					row.Add (value);
+				rows++;
+
+				// Go to the next word
+				while (position < row.RowText.Length && row.RowText [position] != ',')
+					position++;
+				if (position < row.RowText.Length)
+					position++;
+			}
+			// Delete extras
+			while (row.Count > rows)
+				row.RemoveAt (rows);
+
+			return (row.Count > 0);
+		}
+	}
+
+	// Class to write to a csv file
+	public class CsvWriter : StreamWriter
+	{
+		public CsvWriter(string file) : base(file)
+		{
+		}
+		public CsvWriter(Stream stream) : base(stream)
+		{
+		}
+
+		public void WriteRow(CsvRow row)
+		{
+			StringBuilder build = new StringBuilder();
+			bool first = true;
+
+			foreach (string value in row)
+			{
+				if (!first)
+					build.Append (',');
+				build.Append (value);
+				first = false;
+			}
+			row.RowText = build.ToString ();
+			WriteLine (row.RowText);
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,169 @@
+﻿/* 
+ * Code by Brad Wight
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSV_ReadWrite
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//AddWord(2, "dis-(prefix)", "Testing");
+			//DeleteWord(2, "dis-(prefix)", "Testing");
+			GetWords(2, "ng & nk", 12);
+		}
+		// The location of the master CSV file
+		static string filepath = "/home/bradford/Development/c_sharp/CS_Inno1/CSV_ReadWrite/all_levels.csv";
+
+
+		public static void AddWord(int lvl, string listname, string word)
+		{
+			string level = lvl.ToString ();
+			listname = listname.Trim ();
+			word = word.Trim ().ToLower ();
+
+			// Create a File Stream to read from
+			FileStream fs = new FileStream (@filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+			
+			// Add a word to the end of the desired list
+			using (CsvReader reader = new CsvReader(fs))
+			using (CsvWriter writer = new CsvWriter("/home/bradford/Development/c_sharp/CS_Inno1/CSV_ReadWrite/temp.csv"))
+			{
+				CsvRow row = new CsvRow ();
+				while (reader.ReadRow (row)) 
+				{
+					// Locate the list to add the word to
+					if (row [0].Equals (level) && row [1].Equals (listname))
+					{
+						// Check if the word is already in the list
+						foreach (string wrd in row)
+							if (word.Equals(wrd))
+							{
+								Console.WriteLine ("Error: That word is already in the list.");
+								return;
+							}
+						// Remove empty strings, add the word, and write to the file
+						row.RemoveAll (string.IsNullOrWhiteSpace);
+						row.Add (word);
+						writer.WriteRow (row);
+						Console.WriteLine (String.Format("Added word {0} to list {1} for level {2}", word, listname, level));
+					} else
+						// Write the rest of the rows
+						writer.WriteRow (row);	
+				}
+			}
+			File.Delete(filepath);
+
+			// Rename temp.csv to all_levels
+			File.Move("/home/bradford/Development/c_sharp/CS_Inno1/CSV_ReadWrite/temp.csv", filepath);
+		}
+
+
+		public static void DeleteWord(int lvl, string listname, string word)
+		{
+			string level = lvl.ToString ();
+			listname = listname.Trim ();
+			word = word.Trim ().ToLower ();
+
+			// Create a File Stream to read from
+			FileStream fs = new FileStream (@filepath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None);
+
+			// Remove a word from a list
+			using (CsvReader reader = new CsvReader (fs))
+			using (CsvWriter writer = new CsvWriter ("/home/bradford/Development/c_sharp/CS_Inno1/CSV_ReadWrite/temp.csv")) {
+				CsvRow row = new CsvRow ();
+				while (reader.ReadRow (row)) 
+				{
+					// Locate the list to delete the word from
+					if (row [0].Equals (level) && row [1].Equals (listname)) 
+					{
+						// Find the word in the list and remove it
+						for (int i = row.Count - 1; i >= 0; i--)
+							if (row [i].Equals (word))
+								row.RemoveAt (i);
+							
+						// Remove any empty spaces, then write the row
+						row.RemoveAll (string.IsNullOrWhiteSpace);
+						writer.WriteRow (row);
+						Console.WriteLine (String.Format ("Removed word {0} from list {1} for level {2}", word, listname, level));
+					} else
+						// Write the rest of the rows
+						writer.WriteRow (row);
+				}
+			}
+			File.Delete(filepath);
+
+			// Rename temp.csv to all_levels
+			File.Move("/home/bradford/Development/c_sharp/CS_Inno1/CSV_ReadWrite/temp.csv", filepath);					
+		}
+
+		// Get words from a list
+		public static void GetWords(int lvl, string listname, int amount)
+		{
+			string level = lvl.ToString ();
+			listname = listname.Trim ();
+
+			// This list holds the words to display
+			List<string> words = new List<string> ();
+
+			using (CsvReader reader = new CsvReader (filepath))
+			{
+				CsvRow row = new CsvRow ();
+				Random random = new Random ();
+
+				while(reader.ReadRow(row))
+				{
+					// Locate the list to get words from
+					if (row [0].Equals (level) && row [1].Equals (listname))
+					{
+						// Get rid of any extra empty cells
+						row.RemoveAll (string.IsNullOrWhiteSpace);
+
+						// Delete the first two elements because we don't want them in our words
+						row.RemoveAt (1);
+						row.RemoveAt (0);
+
+						// Check if the amount of words requested is more than the list size
+						if (amount > row.Count)
+						{
+							Console.WriteLine (String.Format ("Not enough words in the list {0}. Printing entire list:", listname));
+							foreach (string s in row)
+							{
+								Console.Write(s);
+								Console.Write(" ");
+							}
+						}
+
+						// Find random words
+						for (int i = 0; i < amount; i++) 
+						{
+							begin:
+							int num = random.Next (row.Count);
+							string word = row [num];
+
+							// Make sure it's not already in the list (no duplicates)
+							foreach (string wrd in words)
+								if (word.Equals (wrd))
+									goto begin;
+
+							words.Add (word);
+						}
+
+						// Show the words
+						string amnt = amount.ToString ();
+						Console.WriteLine (String.Format ("{0} words from the list {1} on level {2}:", amnt, listname, level));
+						foreach (string w in words)
+						{
+							Console.Write (w);
+							Console.Write (" ");
+						}
+					}
+				}
+			}
+		}
+	}
+}
