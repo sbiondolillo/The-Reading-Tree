5a4c0f57143dd21cf869efa7866aa6ce56551394
Matthew Brady <matt_brad@outlook.com>
2017-11-08 12:42:29 -0500

    fixed the blends/final digraph + letter blends

--- ReadingTree/BlendsLetterBlendsMenu.Designer.cs
+++ ReadingTree/BlendsLetterBlendsMenu.Designer.cs
@@ -47,9 +47,10 @@
             // 
             // btnSchwaAAlaska
             // 
-            this.btnSchwaAAlaska.Location = new System.Drawing.Point(520, 305);
+            this.btnSchwaAAlaska.Location = new System.Drawing.Point(347, 198);
+            this.btnSchwaAAlaska.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSchwaAAlaska.Name = "btnSchwaAAlaska";
-            this.btnSchwaAAlaska.Size = new System.Drawing.Size(125, 70);
+            this.btnSchwaAAlaska.Size = new System.Drawing.Size(83, 45);
             this.btnSchwaAAlaska.TabIndex = 27;
             this.btnSchwaAAlaska.Text = "schwa a (Alaska)";
             this.btnSchwaAAlaska.UseVisualStyleBackColor = true;
@@ -57,9 +58,10 @@
             // 
             // btnQUASquash
             // 
-            this.btnQUASquash.Location = new System.Drawing.Point(391, 305);
+            this.btnQUASquash.Location = new System.Drawing.Point(261, 198);
+            this.btnQUASquash.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnQUASquash.Name = "btnQUASquash";
-            this.btnQUASquash.Size = new System.Drawing.Size(125, 70);
+            this.btnQUASquash.Size = new System.Drawing.Size(83, 45);
             this.btnQUASquash.TabIndex = 26;
             this.btnQUASquash.Text = "qua (squash)";
             this.btnQUASquash.UseVisualStyleBackColor = true;
@@ -67,9 +69,10 @@
             // 
             // btnOY
             // 
-            this.btnOY.Location = new System.Drawing.Point(262, 305);
+            this.btnOY.Location = new System.Drawing.Point(175, 198);
+            this.btnOY.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOY.Name = "btnOY";
-            this.btnOY.Size = new System.Drawing.Size(125, 70);
+            this.btnOY.Size = new System.Drawing.Size(83, 45);
             this.btnOY.TabIndex = 25;
             this.btnOY.Text = "oy";
             this.btnOY.UseVisualStyleBackColor = true;
@@ -77,9 +80,10 @@
             // 
             // btnWAWater
             // 
-            this.btnWAWater.Location = new System.Drawing.Point(133, 305);
+            this.btnWAWater.Location = new System.Drawing.Point(89, 198);
+            this.btnWAWater.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnWAWater.Name = "btnWAWater";
-            this.btnWAWater.Size = new System.Drawing.Size(125, 70);
+            this.btnWAWater.Size = new System.Drawing.Size(83, 45);
             this.btnWAWater.TabIndex = 24;
             this.btnWAWater.Text = "wa (water)";
             this.btnWAWater.UseVisualStyleBackColor = true;
@@ -87,9 +91,10 @@
             // 
             // btnDGE
             // 
-            this.btnDGE.Location = new System.Drawing.Point(520, 216);
+            this.btnDGE.Location = new System.Drawing.Point(347, 140);
+            this.btnDGE.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDGE.Name = "btnDGE";
-            this.btnDGE.Size = new System.Drawing.Size(125, 70);
+            this.btnDGE.Size = new System.Drawing.Size(83, 45);
             this.btnDGE.TabIndex = 23;
             this.btnDGE.Text = "dge";
             this.btnDGE.UseVisualStyleBackColor = true;
@@ -97,19 +102,21 @@
             // 
             // btnAbleLatinSuffix
             // 
-            this.btnAbleLatinSuffix.Location = new System.Drawing.Point(391, 216);
+            this.btnAbleLatinSuffix.Location = new System.Drawing.Point(261, 140);
+            this.btnAbleLatinSuffix.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAbleLatinSuffix.Name = "btnAbleLatinSuffix";
-            this.btnAbleLatinSuffix.Size = new System.Drawing.Size(125, 70);
+            this.btnAbleLatinSuffix.Size = new System.Drawing.Size(83, 45);
             this.btnAbleLatinSuffix.TabIndex = 22;
-            this.btnAbleLatinSuffix.Text = "able (Latin suffix)";
+            this.btnAbleLatinSuffix.Text = "Able (Latin Suffix)";
             this.btnAbleLatinSuffix.UseVisualStyleBackColor = true;
             this.btnAbleLatinSuffix.Click += new System.EventHandler(this.btnAbleLatinSuffix_Click);
             // 
             // btnOWPlow
             // 
-            this.btnOWPlow.Location = new System.Drawing.Point(262, 216);
+            this.btnOWPlow.Location = new System.Drawing.Point(175, 140);
+            this.btnOWPlow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOWPlow.Name = "btnOWPlow";
-            this.btnOWPlow.Size = new System.Drawing.Size(125, 70);
+            this.btnOWPlow.Size = new System.Drawing.Size(83, 45);
             this.btnOWPlow.TabIndex = 21;
             this.btnOWPlow.Text = "ow (plow)";
             this.btnOWPlow.UseVisualStyleBackColor = true;
@@ -117,9 +124,10 @@
             // 
             // btnOUOut
             // 
-            this.btnOUOut.Location = new System.Drawing.Point(133, 216);
+            this.btnOUOut.Location = new System.Drawing.Point(89, 140);
+            this.btnOUOut.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnOUOut.Name = "btnOUOut";
-            this.btnOUOut.Size = new System.Drawing.Size(125, 70);
+            this.btnOUOut.Size = new System.Drawing.Size(83, 45);
             this.btnOUOut.TabIndex = 20;
             this.btnOUOut.Text = "ou (out)";
             this.btnOUOut.UseVisualStyleBackColor = true;
@@ -127,9 +135,10 @@
             // 
             // btnAll
             // 
-            this.btnAll.Location = new System.Drawing.Point(520, 133);
+            this.btnAll.Location = new System.Drawing.Point(347, 86);
+            this.btnAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAll.Name = "btnAll";
-            this.btnAll.Size = new System.Drawing.Size(125, 70);
+            this.btnAll.Size = new System.Drawing.Size(83, 45);
             this.btnAll.TabIndex = 19;
             this.btnAll.Text = "all";
             this.btnAll.UseVisualStyleBackColor = true;
@@ -137,9 +146,10 @@
             // 
             // btnALALLMixed
             // 
-            this.btnALALLMixed.Location = new System.Drawing.Point(391, 133);
+            this.btnALALLMixed.Location = new System.Drawing.Point(261, 86);
+            this.btnALALLMixed.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnALALLMixed.Name = "btnALALLMixed";
-            this.btnALALLMixed.Size = new System.Drawing.Size(125, 70);
+            this.btnALALLMixed.Size = new System.Drawing.Size(83, 45);
             this.btnALALLMixed.TabIndex = 18;
             this.btnALALLMixed.Text = "al/all mixed";
             this.btnALALLMixed.UseVisualStyleBackColor = true;
@@ -147,9 +157,10 @@
             // 
             // btnSEasZ
             // 
-            this.btnSEasZ.Location = new System.Drawing.Point(262, 133);
+            this.btnSEasZ.Location = new System.Drawing.Point(175, 86);
+            this.btnSEasZ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSEasZ.Name = "btnSEasZ";
-            this.btnSEasZ.Size = new System.Drawing.Size(125, 70);
+            this.btnSEasZ.Size = new System.Drawing.Size(83, 45);
             this.btnSEasZ.TabIndex = 17;
             this.btnSEasZ.Text = "se as /z/";
             this.btnSEasZ.UseVisualStyleBackColor = true;
@@ -157,9 +168,10 @@
             // 
             // btnQU
             // 
-            this.btnQU.Location = new System.Drawing.Point(133, 133);
+            this.btnQU.Location = new System.Drawing.Point(89, 86);
+            this.btnQU.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnQU.Name = "btnQU";
-            this.btnQU.Size = new System.Drawing.Size(125, 70);
+            this.btnQU.Size = new System.Drawing.Size(83, 45);
             this.btnQU.TabIndex = 16;
             this.btnQU.Text = "qu";
             this.btnQU.UseVisualStyleBackColor = true;
@@ -171,9 +183,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.BlendsLetterBlendsPromptlabel.AutoSize = true;
-            this.BlendsLetterBlendsPromptlabel.Location = new System.Drawing.Point(233, 66);
+            this.BlendsLetterBlendsPromptlabel.Location = new System.Drawing.Point(155, 43);
+            this.BlendsLetterBlendsPromptlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.BlendsLetterBlendsPromptlabel.Name = "BlendsLetterBlendsPromptlabel";
-            this.BlendsLetterBlendsPromptlabel.Size = new System.Drawing.Size(313, 20);
+            this.BlendsLetterBlendsPromptlabel.Size = new System.Drawing.Size(213, 13);
             this.BlendsLetterBlendsPromptlabel.TabIndex = 15;
             this.BlendsLetterBlendsPromptlabel.Text = "Please select a category below to continue!";
             // 
@@ -183,17 +196,19 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.BlendsLetterBlendslabel.AutoSize = true;
-            this.BlendsLetterBlendslabel.Location = new System.Drawing.Point(309, 24);
+            this.BlendsLetterBlendslabel.Location = new System.Drawing.Point(206, 16);
+            this.BlendsLetterBlendslabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.BlendsLetterBlendslabel.Name = "BlendsLetterBlendslabel";
-            this.BlendsLetterBlendslabel.Size = new System.Drawing.Size(161, 20);
+            this.BlendsLetterBlendslabel.Size = new System.Drawing.Size(107, 13);
             this.BlendsLetterBlendslabel.TabIndex = 14;
             this.BlendsLetterBlendslabel.Text = "Blends, Letter Blends";
             // 
             // btnMainMenu
             // 
-            this.btnMainMenu.Location = new System.Drawing.Point(327, 400);
+            this.btnMainMenu.Location = new System.Drawing.Point(218, 260);
+            this.btnMainMenu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMainMenu.Name = "btnMainMenu";
-            this.btnMainMenu.Size = new System.Drawing.Size(125, 70);
+            this.btnMainMenu.Size = new System.Drawing.Size(83, 45);
             this.btnMainMenu.TabIndex = 28;
             this.btnMainMenu.Text = "Main Menu";
             this.btnMainMenu.UseVisualStyleBackColor = true;
@@ -201,10 +216,10 @@
             // 
             // BlendsLetterBlendsMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.ClientSize = new System.Drawing.Size(519, 354);
             this.Controls.Add(this.btnMainMenu);
             this.Controls.Add(this.btnSchwaAAlaska);
             this.Controls.Add(this.btnQUASquash);
@@ -220,6 +235,7 @@
             this.Controls.Add(this.btnQU);
             this.Controls.Add(this.BlendsLetterBlendsPromptlabel);
             this.Controls.Add(this.BlendsLetterBlendslabel);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "BlendsLetterBlendsMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Reading Tree";
