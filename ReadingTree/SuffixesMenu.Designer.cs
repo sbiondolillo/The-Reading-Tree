1d42fa4c57c897a4cf61cac1235591ca06799560
Samuel Biondolillo <biwinning121@gmail.com>
2017-11-14 10:19:10 -0500

    finish Suffix, Roots, OO Sounds

--- ReadingTree/SuffixesMenu.Designer.cs
+++ ReadingTree/SuffixesMenu.Designer.cs
@@ -114,6 +114,8 @@
             this.Name = "SuffixesMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Reading Tree";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_Closing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Closed);
             this.ResumeLayout(false);
             this.PerformLayout();
 
