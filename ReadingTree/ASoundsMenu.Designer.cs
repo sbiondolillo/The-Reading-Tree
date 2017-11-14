4ab4d05c9c58f6b61a87ce86419221a97f393ba0
Samuel Biondolillo <biwinning121@gmail.com>
2017-11-14 09:28:09 -0500

    initial test case passing

--- ReadingTree/ASoundsMenu.Designer.cs
+++ ReadingTree/ASoundsMenu.Designer.cs
@@ -159,6 +159,8 @@
             this.Name = "ASoundsMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "The Reading Tree";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ASoundsMenu_OnFormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ASoundsMenu_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ReadingTree/ASoundsMenu.cs
+++ ReadingTree/ASoundsMenu.cs
@@ -12,9 +12,11 @@ namespace ReadingTree
 {
     public partial class ASoundsMenu : Form
     {
+        private bool userClosed { get; set; }
         public ASoundsMenu()
         {
             InitializeComponent();
+            userClosed = false;
         }
 
         private void btnAESounds_Click(object sender, EventArgs e)
@@ -75,6 +77,22 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-       
+        
+        private void ASoundsMenu_OnFormClosing(object sender, FormClosingEventArgs e)
+        {
+            /// tests if the form was closed by the Close() method or by the user clicking the 'X' button
+            /// if the user clicks the 'X' button, we want to exit the application, otherwise, proceed as normal
+            /// adapted from https://stackoverflow.com/questions/13361260/how-to-distinguish-window-close-button-clicked-x-vs-window-close-in-closi
+            bool closedWithXButton = new System.Diagnostics.StackTrace().GetFrames().FirstOrDefault(x => x.GetMethod() == typeof(Form).GetMethod("Close")) == null;
+            if (closedWithXButton)
+            {
+                userClosed = true;
+            }
+        }
+        private void ASoundsMenu_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (userClosed)
+                Application.Exit();
+        }
     }
 }
