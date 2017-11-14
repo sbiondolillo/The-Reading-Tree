22822548b2af6aea2fa166d6f53437be4ebb657c
Samuel Biondolillo <biwinning121@gmail.com>
2017-11-14 10:25:01 -0500

    finish VC Variables, Digraph, Consonant Manipulation

--- ReadingTree/VCVariablesMenu.cs
+++ ReadingTree/VCVariablesMenu.cs
@@ -12,18 +12,17 @@ namespace ReadingTree
 {
     public partial class VCVariablesMenu : Form
     {
+        private bool userClosed { get; set; } = false;
         public VCVariablesMenu()
         {
             InitializeComponent();
         }
-
         private void btnMainMenu_Click(object sender, EventArgs e)
         {
             MainMenu mainMenu = new MainMenu();
             mainMenu.Show();
             Close();
         }
-
         private void btnVCCVE_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -32,7 +31,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -41,7 +39,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCCV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -50,7 +47,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVC_CCCV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -59,7 +55,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVC_CCV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -68,7 +63,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCC_CCV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -77,7 +71,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCC_CV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -86,7 +79,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCCC_CV_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -95,7 +87,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCCCCVmixed_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -104,7 +95,6 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btnVCCCVmixed_Click(object sender, EventArgs e)
         {
             History.SetPrev(this);
@@ -113,12 +103,27 @@ namespace ReadingTree
             level.Show();
             Hide();
         }
-
         private void btn_back_Click(object sender, EventArgs e)
         {
             System.Windows.Forms.Form previous = History.GetPrev();
             previous.Show();
             Close();
+        }
+        private void Form_Closing(object sender, FormClosingEventArgs e)
+        {
+            /// tests if the form was closed by the Close() method or by the user clicking the 'X' button
+            /// if the user clicks the 'X' button, we want to exit the application, otherwise, proceed as normal
+            /// adapted from https://stackoverflow.com/questions/13361260/how-to-distinguish-window-close-button-clicked-x-vs-window-close-in-closi
+            bool closedWithXButton = new System.Diagnostics.StackTrace().GetFrames().FirstOrDefault(x => x.GetMethod() == typeof(Form).GetMethod("Close")) == null;
+            if (closedWithXButton)
+            {
+                userClosed = true;
+            }
+        }
+        private void Form_Closed(object sender, FormClosedEventArgs e)
+        {
+            if (userClosed)
+                Application.Exit();
         }
     }
 }
